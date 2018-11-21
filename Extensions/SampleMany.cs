b2261940f24962f5ebb1894674a2ca6919595192
glopesdev <glopesdev@users.noreply.github.com>
2018-11-21 16:28:09 +0000

    Added preliminary version of SampleMany operator

---
+++ Extensions/SampleMany.cs
@@ -0,0 +1,31 @@
+using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using MathNet.Numerics.Distributions;
+
+[Combinator]
+[Description("Draws multiple random samples from the input distribution.")]
+[WorkflowElementCategory(ElementCategory.Combinator)]
+public class SampleMany
+{
+    public SampleMany()
+    {
+        Count = 1;
+    }
+
+    [Description("The number of random samples to draw.")]
+    public int Count { get; set; }
+
+    public IObservable<int> Process(IObservable<IDiscreteDistribution> source)
+    {
+        return source.SelectMany(distribution => distribution.Samples().Take(Count));
+    }
+
+    public IObservable<double> Process(IObservable<IContinuousDistribution> source)
+    {
+        return source.SelectMany(distribution => distribution.Samples().Take(Count));
+    }
+}
