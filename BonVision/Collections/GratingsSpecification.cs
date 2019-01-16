bc0556b474083a07a48bc09a8e39c53b31f67fb6
glopesdev <glopesdev@users.noreply.github.com>
2019-01-16 13:56:21 +0000

    Moved gratings specification to the collections category

--- BonVision/Collections/GratingsSpecification.cs
+++ BonVision/Collections/GratingsSpecification.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reactive.Linq;
 
-namespace BonVision
+namespace BonVision.Collections
 {
     public class GratingParameters
     {
