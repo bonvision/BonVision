6dacb44fcc9764026f8da904daba445f4cd72715
glopesdev <glopesdev@users.noreply.github.com>
2019-10-06 16:47:25 +0100

    Improved usability of gratings specification

--- BonVision/Collections/GratingsSpecification.cs
+++ BonVision/Collections/GratingsSpecification.cs
@@ -23,16 +23,37 @@ namespace BonVision.Collections
 
         public float? TemporalFrequency { get; set; }
 
+        [Range(-Math.PI, Math.PI)]
+        [TypeConverter(typeof(NullableDegreeConverter))]
+        [Editor(DesignTypes.SliderEditor, DesignTypes.UITypeEditor)]
         public float? Orientation { get; set; }
+
+        public override string ToString()
+        {
+            var parameters = new List<string>();
+            const string PropertySeparator = ": ";
+            const string ParameterSeparator = ", ";
+            if (Duration.HasValue) parameters.Add(nameof(Duration) + PropertySeparator + Duration.Value);
+            if (Diameter.HasValue) parameters.Add(nameof(Diameter) + PropertySeparator + Diameter.Value);
+            if (X.HasValue) parameters.Add(nameof(X) + PropertySeparator + X.Value);
+            if (Y.HasValue) parameters.Add(nameof(Y) + PropertySeparator + Y.Value);
+            if (Contrast.HasValue) parameters.Add(nameof(Contrast) + PropertySeparator + Contrast.Value);
+            if (SpatialFrequency.HasValue) parameters.Add(nameof(SpatialFrequency) + PropertySeparator + SpatialFrequency.Value);
+            if (TemporalFrequency.HasValue) parameters.Add(nameof(TemporalFrequency) + PropertySeparator + TemporalFrequency.Value);
+            if (Orientation.HasValue) parameters.Add(nameof(Orientation) + PropertySeparator + DegreeConverter.RadianToDegree(Orientation.Value) + "°");
+            return parameters.Count > 0 ? string.Join(ParameterSeparator, parameters) : nameof(GratingParameters);
+        }
     }
 
     [Combinator]
+    [DefaultProperty(nameof(Trials))]
     [Description("Creates a sequence of grating parameters used for stimulus presentation.")]
     [WorkflowElementCategory(ElementCategory.Source)]
     public class GratingsSpecification
     {
         private List<GratingParameters> trials = new List<GratingParameters>();
 
+        [Description("The trial sequence of grating parameters used for stimulus presentation.")]
         public List<GratingParameters> Trials
         {
             get { return trials; }
