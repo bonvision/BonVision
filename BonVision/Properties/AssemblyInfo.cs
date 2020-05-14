783f0aa20d6fadf3526506548a17a0bdb2ed9be9
glopesdev <glopesdev@users.noreply.github.com>
2020-05-14 20:32:14 +0100

    Update package project to the new csproj format

--- BonVision/Properties/AssemblyInfo.cs
+++ BonVision/Properties/AssemblyInfo.cs
@@ -1,38 +1,6 @@
 ﻿using Bonsai;
-using System.Reflection;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BonVision")]
-[assembly: AssemblyDescription("A visual stimulus generator and environmental mapping package for Bonsai.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Aman Saleem")]
-[assembly: AssemblyProduct("BonVision")]
-[assembly: AssemblyCopyright("Copyright © Aman Saleem 2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c9d503fd-bef2-4493-918c-b8572947b7d4")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.0.0")]
-[assembly: AssemblyFileVersion("0.6.0.0")]
-[assembly: AssemblyInformationalVersion("0.6.0")]
 [assembly: XmlNamespacePrefix("clr-namespace:BonVision", "bv")]
